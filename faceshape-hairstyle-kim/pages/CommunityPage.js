092ca4e143ac0e1f426629f6dff8960f9610a16f
doyeon <mju1025@naver.com>
2021-05-26 15:49:10 +0900

    communitypage ui update

--- faceshape-hairstyle-kim/pages/CommunityPage.js
+++ faceshape-hairstyle-kim/pages/CommunityPage.js
@@ -66,11 +66,11 @@ const isFocused = useIsFocused()
             <ScrollView style={styles.faceshapeButton} horizontal indicatorStyle={"white"}>
                 <TouchableOpacity style={styles.allButton} onPress={()=>{category('전체보기')}}><Text style={styles.allText}>전체보기</Text></TouchableOpacity>
                 <TouchableOpacity style={styles.eggButton} onPress={()=>{category('egg')}}><Text style={styles.eggText}>계란형</Text></TouchableOpacity>
-                <TouchableOpacity style={styles.diamondButton} onPress={()=>{category('diamond')}}><Text style={styles.diamondText}>마름모형</Text></TouchableOpacity>
+                <TouchableOpacity style={styles.diamondButton} onPress={()=>{category('rhombus')}}><Text style={styles.diamondText}>마름모형</Text></TouchableOpacity>
                 <TouchableOpacity style={styles.heartButton} onPress={()=>{category('heart')}}><Text style={styles.heartText}>하트형</Text></TouchableOpacity>
                 <TouchableOpacity style={styles.peanutButton} onPress={()=>{category('peanut')}}><Text style={styles.peanutText}>땅콩형</Text></TouchableOpacity>
-                <TouchableOpacity style={styles.circleButton} onPress={()=>{category('circle')}}><Text style={styles.circleText}>둥근형</Text></TouchableOpacity>
-                <TouchableOpacity style={styles.sixButton} onPress={()=>{category('six')}}><Text style={styles.sixText}>육각형</Text></TouchableOpacity>
+                <TouchableOpacity style={styles.circleButton} onPress={()=>{category('round')}}><Text style={styles.circleText}>둥근형</Text></TouchableOpacity>
+                <TouchableOpacity style={styles.sixButton} onPress={()=>{category('hexagon')}}><Text style={styles.sixText}>육각형</Text></TouchableOpacity>
             </ScrollView>
             </View>
 
@@ -120,89 +120,137 @@ const styles = StyleSheet.create({
     },
     faceshapeButtonC:{        
         
-        height: 37,
+        height: 45,
     },
     faceshapeButton:{
-        backgroundColor: "#8f8f8f"
+        backgroundColor: "#ffffff"
     },
     allButton:{
-        width: 72,
-        height: 25,
+        marginLeft:7,
+        marginTop:6,
+        width: 75,
+        height: 33,
+        borderColor:"#8f8f8f",
+        borderWidth:1.7,
+        backgroundColor: "#ffffff",
+        borderRadius:30
     },
     allText:{
-        fontSize: 12,
-        color: "#ffffff",
+        fontWeight:"bold",
+        fontSize: 13,
+        color: "#000000",
         textAlign:"center",
         marginTop:7
     },
     eggButton:{
-        width: 72,
-        height: 25,
+        marginLeft:7,
+        marginTop:6,
+        width: 75,
+        height: 33,
+        borderColor:"#8f8f8f",
+        borderWidth:1.7,
+        backgroundColor: "#ffffff",
+        borderRadius:30
     },
     eggText:{
-        fontSize: 12,
-        color: "#ffffff",
+        fontWeight:"bold",
+        fontSize: 13,
+        color: "#000000",
         textAlign:"center",
         marginTop:7
     },
     diamondButton:{
-        width: 72,
-        height: 25,
+        marginLeft:7,
+        marginTop:6,
+        width: 75,
+        height: 33,
+        borderColor:"#8f8f8f",
+        borderWidth:1.7,
+        backgroundColor: "#ffffff",
+        borderRadius:30
     },
     diamondText:{
-        fontSize: 12,
-        color: "#ffffff",
+        fontWeight:"bold",
+        fontSize: 13,
+        color: "#000000",
         textAlign:"center",
         marginTop:7
     },
     heartButton:{
-        width: 72,
-        height: 25,
+        marginLeft:7,
+        marginTop:6,
+        width: 75,
+        height: 33,
+        borderColor:"#8f8f8f",
+        borderWidth:1.7,
+        backgroundColor: "#ffffff",
+        borderRadius:30
     },
     heartText:{
-        fontSize: 12,
-        color: "#ffffff",
+        fontWeight:"bold",
+        fontSize: 13,
+        color: "#000000",
         textAlign:"center",
         marginTop:7
     },
     peanutButton:{
-        width: 72,
-        height: 25,
+        marginLeft:7,
+        marginTop:6,
+        width: 75,
+        height: 33,
+        borderColor:"#8f8f8f",
+        borderWidth:1.7,
+        backgroundColor: "#ffffff",
+        borderRadius:30
     },
     peanutText:{
-        fontSize: 12,
-        color: "#ffffff",
+        fontWeight:"bold",
+        fontSize: 13,
+        color: "#000000",
         textAlign:"center",
         marginTop:7
     },
     circleButton:{
-        width: 72,
-        height: 25,
+        marginLeft:7,
+        marginTop:6,
+        width: 75,
+        height: 33,
+        borderColor:"#8f8f8f",
+        borderWidth:1.7,
+        backgroundColor: "#ffffff",
+        borderRadius:30
     },
     circleText:{
-        fontSize: 12,
-        color: "#ffffff",
+        fontWeight:"bold",
+        fontSize: 13,
+        color: "#000000",
         textAlign:"center",
         marginTop:7
     },
     sixButton:{
-        width: 72,
-        height: 25,
+        marginLeft:7,
+        marginTop:6,
+        width: 75,
+        height: 33,
+        borderColor:"#8f8f8f",
+        borderWidth:1.7,
+        backgroundColor: "#ffffff",
+        borderRadius:30
     },
     sixText:{
-        fontSize: 12,
-        color: "#ffffff",
+        fontWeight:"bold",
+        fontSize: 13,
+        color: "#000000",
         textAlign:"center",
         marginTop:7
     },
     communityCard:{
-        
         height: 480,
         backgroundColor: "#ffffff",
     },
     communityCardContainer:{
-        borderBottomColor:"#8f8f8f",
-        borderBottomWidth:3
+       // borderTopColor:"#8f8f8f",
+        //borderTopWidth:1
     },
     
     //글쓰기 버튼
