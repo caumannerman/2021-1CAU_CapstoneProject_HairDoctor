ea67b04f6d2bb3bd32f998591d6928fc83d6fa9b
leejingyu <jklee9512@naver.com>
2021-06-01 23:22:22 +0900

    update final_demo code

--- facelib/facetype.js
+++ facelib/facetype.js
@@ -1,43 +1,61 @@
 const db = require('../lib/db')
 const fs = require('fs')
+const sharp = require('sharp')
 var py_Shell = require('python-shell');
 
 var options = {
-    mode: 'text',               // python 파일을 열 모드 text 나 json중에서 선택
-    pythonPath: "/usr/bin/python3",    //ubuntu path
+    mode: 'text',
+    pythonPath: "/usr/bin/python3",
     pythonOptions: ['-u'],
     scriptPath:'../serverbackup/facelib/'
 };
 
+const imagePath = '../serverbackup/facelib'
+
 exports.type = function(req, res) {
     console.log('사진 저장')
-    fs.unlink(`../serverbackup/images/uploads/test.jpg`)
-    var img = Buffer.from(req.body.imgsource, 'base64');
+    fs.unlink(`../serverbackup/facelib/test.jpg`, (err) => {})
+    var img = Buffer.from(req.body.imgsource, 'base64');   
+    const imageTime = Date.now()
+    const original = imagePath + String(imageTime) + '.jpg'
+    fs.writeFile(original, img, (err) => {
+        if(err) console.log(err)
+        sharp(original).resize({width: 400}).toFile('../serverbackup/facelib/test.jpg',(err) => {
+            if(err) console.log(err)
+            fs.unlink('../serverbackup/facelib/original.jpg', err => {})
+        })
+    })
+    
     setTimeout(() => {
-        fs.writeFile('../serverbackup/facelib/test.jpg', img, function(error){
-            if(error){
-                throw error;
-            } else{
-                console.log('File created from base64 string!');
-                py_Shell.PythonShell.run("Mid_Demo.py", options, function(err,results){  
-                    if(err) {
-                        console.log('err msg : error in running python');
-                    } 
-                    else {
-                        console.log(results)
-                        db.query(
-                            `UPDATE user set facetype = '${results}' where user_id =${req.body.user_id};`,
-                            (err, rows, fields) => {
-                                if(err){
-                                    res.status(300).send({state: "error"});
-                                } else {
-                                    console.log('send data')
-                                    res.status(200).send({state: "ok"});
-                                }
-                            }      
-                        )
-                    }
-                })
+        console.log('python start')
+        py_Shell.PythonShell.run("final_test_min.py", options, function(err,results){  
+            if(err) {
+                console.log(err);
+                db.query(
+                    `UPDATE user set facetype = 'error' where user_id =${req.body.user_id};`,
+                    (err, rows, fields) => {
+                        if(err){
+                            res.status(300).send({state: "error"});
+                        } else {
+                            console.log('send data')
+                            res.status(200).send({state: "ok"});
+                        }
+                    }      
+                )
+            } 
+            else {
+                console.log('results msg :', results)
+                db.query(
+                    `UPDATE user set facetype = '${results}' where user_id =${req.body.user_id};`,
+                    (err, rows, fields) => {
+                        if(err){
+                            res.status(300).send({state: "error"});
+                        } else {
+                            console.log('send data')
+                            res.status(200).send({state: "ok"});
+                        }
+                    }      
+                )
             }
         })
     }, 2000)
@@ -47,7 +65,7 @@ exports.face = function(req, res) {
     db.query(
         `select facetype FROM user where user_id =${req.body.user_id};`,
         (err, rows, fields) => {
-            //console.log(rows)
+            console.log('user facetype =', rows[0])
             res.send(rows[0])
         }      
     )
@@ -55,14 +73,8 @@ exports.face = function(req, res) {
 
 exports.photo = function(req, res) {
     console.log(`ML result img return to client`)
-    fs.readdir('../serverbackup/images/uploads', function(err, filelist){
-        console.log('filelist:', filelist)
-        if(filelist[0] == null){
-            console.log('no file')
-            res.send( {uri :`https://hairdoctor.owlsogul.com/component/noimage.png`});
-        }
-        else {
-            res.send( {uri :`https://hairdoctor.owlsogul.com/uploads/test.jpg`});
-        }
-    })
+    res.send( {uri :`https://hairdoctor.owlsogul.com/uploads/test.jpg`});
+    setTimeout(() => {
+        fs.unlink(`../serverbackup/images/uploads/test.jpg`, err => {})
+    },20000)
 }
